443f06b5084117d7609c5aefb23da9d4fd07860d
lfrbmw <100558749+lfrbmw@users.noreply.github.com>
2025-06-12 23:30:46 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/147933545",
-    "https://blog.csdn.net/lwcwam/article/details/147933545",
-    "https://lucianaib.blog.csdn.net/article/details/148077253"
+    "https://blog.csdn.net/lwcwam/article/details/148570281",
+    "https://blog.csdn.net/lwcwam/article/details/148521093",
+    "https://blog.csdn.net/lwcwam/article/details/148503104"
 ]
