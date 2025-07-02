51f3addcfc1b1b62359204a4f16e044c4cf2ce23
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-07-02 15:37:25 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://lucianaib.blog.csdn.net/article/details/149033176",
-    "https://lucianaib.blog.csdn.net/article/details/149033176",
+    "https://blog.csdn.net/lwcwam/article/details/149034681",
+    "https://blog.csdn.net/lwcwam/article/details/149034681",
     "https://blog.csdn.net/lwcwam/article/details/148855744"
 ]
