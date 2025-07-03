7076dfe6730f14b615afdc23a862d3801b7525ab
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-07-03 14:20:02 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/149034681",
-    "https://blog.csdn.net/lwcwam/article/details/149034681",
-    "https://blog.csdn.net/lwcwam/article/details/148855744"
+    "https://blog.csdn.net/2401_82648291/article/details/149029029",
+    "https://blog.csdn.net/2401_82648291/article/details/149029029",
+    "https://blog.csdn.net/lwcwam/article/details/149034681"
 ]
