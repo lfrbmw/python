a728908ff3540f625388cbff56561947a972362f
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-07-04 22:04:07 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/148855744",
-    "https://blog.csdn.net/lwcwam/article/details/149034681",
+    "https://blog.csdn.net/lwcwam/article/details/149102735",
+    "https://blog.csdn.net/lwcwam/article/details/149102735",
     "https://blog.csdn.net/lwcwam/article/details/149034681"
 ]
