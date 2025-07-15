2dd5013194233cc4d14af3c068f7c5deb95ac27d
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-07-15 12:36:26 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/146542497",
-    "https://blog.csdn.net/lwcwam/article/details/149160671",
-    "https://blog.csdn.net/lwcwam/article/details/149300757"
+    "https://hyk52syx.blog.csdn.net/article/details/149344564",
+    "https://hyk52syx.blog.csdn.net/article/details/149344564",
+    "https://hyk52syx.blog.csdn.net/article/details/149344564"
 ]
