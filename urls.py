fc13fe2f5563904456deb523bf284220fda98d1b
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-07-20 17:18:43 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/149034681",
-    "https://blog.csdn.net/lwcwam/article/details/149034681",
+    "https://lucianaib.blog.csdn.net/article/details/149034681",
+    "https://lucianaib.blog.csdn.net/article/details/149034681",
     "https://blog.csdn.net/lwcwam/article/details/149034681"
 ]
