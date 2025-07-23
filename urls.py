01fe8c401b025cc01d3b6cc9159a71c3bc6c099e
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-07-23 11:23:18 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://lucianaib.blog.csdn.net/article/details/149034681",
-    "https://lucianaib.blog.csdn.net/article/details/149034681",
-    "https://blog.csdn.net/lwcwam/article/details/149034681"
+    "https://blog.csdn.net/lwcwam/article/details/149564302",
+    "https://blog.csdn.net/lwcwam/article/details/149564302",
+    "https://blog.csdn.net/lwcwam/article/details/149564302"
 ]
