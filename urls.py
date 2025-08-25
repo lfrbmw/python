21f6103e18986a8e5c6758d303a5d8aa133d7421
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-08-25 23:19:03 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/150026404",
-    "https://blog.csdn.net/lwcwam/article/details/150026404",
-    "https://blog.csdn.net/lwcwam/article/details/150026404"
+    "https://blog.csdn.net/lwcwam/article/details/150793228",
+    "https://blog.csdn.net/lwcwam/article/details/150793228",
+    "https://blog.csdn.net/lwcwam/article/details/150793228"
 ]
