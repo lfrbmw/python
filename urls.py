0b80d55798ca70e4c53c805d538931102e7c4db5
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-09-14 08:43:15 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/151248165",
-    "https://blog.csdn.net/lwcwam/article/details/151248165",
-    "https://blog.csdn.net/lwcwam/article/details/151248165"
+    "https://blog.csdn.net/lwcwam/article/details/151614187",
+    "https://blog.csdn.net/lwcwam/article/details/151614187",
+    "https://blog.csdn.net/lwcwam/article/details/151614187"
 ]
