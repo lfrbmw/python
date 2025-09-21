669c566916a17fb6b830579c31e15205ed6d96da
南离 <100558749+lfrbmw@users.noreply.github.com>
2025-09-21 12:52:36 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/lwcwam/article/details/151791886",
-    "https://blog.csdn.net/lwcwam/article/details/151791886",
+    "https://lucianaib.blog.csdn.net/article/details/151803334",
+    "https://lucianaib.blog.csdn.net/article/details/151803334",
     "https://lucianaib.blog.csdn.net/article/details/151803334"
 ]
